cdf8d0745c1bcac4fdc6820c54201dff2039d982
yayaluft <syanuha66@gmail.com>
2026-03-04 22:33:37 +0700

    Percobaan 3

---
+++ matakuliah10.java
@@ -0,0 +1,13 @@
+public class matakuliah10 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public matakuliah10 (String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
